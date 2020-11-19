432931e8dd5bafc19cab4b546291e740273513fb
Pedro Diogo Figueiredo <up201904675@fe.up.pt>
2020-11-19 15:10:25 +0000

    client.h and employee.h fully commented (Doxygen)

--- client.h
+++ client.h
@@ -5,6 +5,8 @@
 #include <ostream>
 #include "vehicle.h"
 
+///Contains info about a client, such as name, nif and his/her vehicles
+
 class Client {
 private:
     string name;
@@ -12,36 +14,84 @@ private:
     vector<Vehicle *> vehicles;
 
 public:
+    ///Creates a Client with name and nif
+    ///
+    /// \param name
+    /// \param nif
     Client(string name, int nif);
 
+    ///Creates a Client with only nif and no assigned name
+    ///
+    /// \param nif
     Client(int nif);
 
+    ///Returns Client's name
+    ///
+    /// \return Client's name
     const string &getName() const;
 
-
-
+    ///Returns Client's nif
+    ///
+    /// \return Client's nif
     int getNif() const;
 
+    ///Returns pointer vector with Client's Vehicles
+    ///
+    /// \return vector with Client's Vehicles
     vector<Vehicle *> &getVehicles() ;
 
+    ///Returns pointer to Client's Vehicle with this licensePlate
+    ///
+    /// \param licensePlate is a string in the format XX-XX-XX
+    /// \return pointer to Client's Vehicle with this licensePlate
     Vehicle *getVehicle(string &licensePlate);
 
+    ///Removes Vehicle from Client's vehicles
+    ///
+    /// \param vehicle is a pointer to the Client's Vehicle which will be removed
     void removeVehicle(Vehicle *vehicle);
 
+    ///Returns true, if the nif's are the same, false otherwise
+    ///
+    /// \param rhs is another Client
+    /// \return true, if the nif's are the same, false otherwise
     bool operator==(const Client &rhs) const;
 
-    bool operator!=(const Client &rhs) const;
+    ///Returns true, if the nif's are different, false otherwise
+    ///
+    /// \param rhs is another Client
+    /// \return true, if the nif's are different, false otherwise
+    bool operator!=(const Client &rhs) const;//REMOVE?
 
+    /// Adds Vehicle to Client's vehicles
+    /// \param vehicle is a pointer to the new Vehicle
     void addVehicle(Vehicle *vehicle);
 
+    ///Prints the info about each of the Client's Vehicles
+    ///
+    ///This function calls Vehicle::printInfo() for each of the Client's Vehicles
+    ///@see Vehicle::printInfo()
     void printVehicles();
 
+    /// Changes Client's name
+    /// \param name string with the Client's new name
     void changeName(string name);
 
+    /// Changes Client's nif
+    /// \param nif int with new nif
+    ///@note Used to correct attribution mistake
     void changeNIF(int nif);
 
+    ///Prints Client's Info
+    ///
+    ///Prints Client's name, nif and number of Vehicles
     void printInfo();
 
+    ///Returns ostream with name, nif, and Vehicle(s) licence plate(s)
+    ///
+    /// \param os
+    /// \param client
+    /// \return
     friend ostream &operator<<(ostream &os, const Client &client);
 };
 
--- employee.h
+++ employee.h
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+///Contains info about an employee, such as name, ss_number and working status
+///
+///The private member "working" will be 1 if the employee is working, or 0 otherwize
+
 class Employee {
 private:
     string name;
@@ -13,22 +17,44 @@ private:
     bool working; //IF EMPLOYEE IS WORKING IN A LANE, VALUE TRUE
 
 public:
+    ///Creates an Employee with name and ss_number
     Employee(string n, int ss): name(n), ss_number(ss){
         working = false;
     };
 
     ~Employee(){};
 
+    ///Returns Employee's name
+    ///
+    /// \return Employee's name
     const string &getName() const;
 
+    ///Returns Employee's ss number
+    ///
+    /// \return Employee's ss number
     int getSsNumber() const;
 
+    ///Returns true, if ss_numbers are equal, false otherwise
+    ///
+    /// \param rhs is another Employee
+    /// \return true, if ss_numbers are equal, false otherwise
     bool operator==(const Employee &rhs) const;
 
+    ///Returns true, if the Employee is working, false otherwise
+    ///
+    /// \return true, if the Employee is working, false otherwise
     bool isWorking();
 
+    ///Changes the boolean member working
+    ///
+    ///If working is true it becomes false and vice-versa
     void changeWorkStatus(){working=!working;};
 
+    ///Returns ostream with name and ss_number
+    ///
+    /// \param os
+    /// \param employee
+    /// \return ostream with Employee's name and ss number
     friend ostream &operator<<(ostream &os, const Employee &employee);
 };
 
